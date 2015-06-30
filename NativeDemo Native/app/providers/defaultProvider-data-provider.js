1d78dd2f01338703f8059aede3c3a6fe06380060
Lakshman Sha <lakshmansha1992@gmail.com>
2015-06-30 06:07:28 +0000

    Changes from Screen Builder

---
+++ NativeDemo Native/app/providers/defaultProvider-data-provider.js
@@ -0,0 +1,10 @@
+var provider,
+    TelerikBackendServices = require('../everlive/everlive.all.min');
+
+provider = new TelerikBackendServices({
+
+    apiKey: 'vHav5riKi5AqSbnf',
+    scheme: 'https'
+});
+
+module.exports = provider;
